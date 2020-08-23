d4f5653102616828a21a872300d1446fc53b5029
elywin <lukeeraelywin@gmail.com>
2020-08-24 00:33:58 +0300

    ch(add:created verification functions for the connection)

--- index.js
+++ index.js
@@ -2,7 +2,16 @@
 const express = require("express");
 const mongoose =require("mongoose");
 const url = 'mongodb://localhost/testdb';
+const db =mongoose.connection;
 
 //connect mongodb with connect method
 mongoose.connect(url,{useNewUrlParser:true});
 
+//veryfying connection
+db.once('open',_=>{
+    console.log('databse connected:', url);
+});
+
+db.on('error',(err)=>{
+    console.error('connection failed:',err);
+});
