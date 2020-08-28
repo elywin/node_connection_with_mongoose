0f7104d44cc49c3a6cb62f3bf1665246d4e95542
elywin <lukeeraelywin@gmail.com>
2020-08-28 12:08:35 +0300

    ch(add:ignored node modules)

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 //imported required packages
 const express = require("express");
-const mongoose =require("mongoose");
+const mongoose =require('mongoose');
 const url = 'mongodb://localhost/testdb';
-const db =mongoose.connection;
+// const db =mongoose.connection;
+const app =express();
 
 //connect mongodb with connect method
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true });
-
+const db = mongoose.connection;
 //veryfying connection
 db.once('open',_=>{
     console.log('databse connected:', url);
@@ -16,3 +17,27 @@ db.on('error',(err)=>{
     console.error('connection failed:',err);
 });
 
+app.get('/',(req,res)=>{
+    res.send('hello world!');
+}).listen(2001,_=>{
+    console.log('server running on http://localhost:2001');
+});
+
+
+//organise data //create schema 
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    }
+});
+
+//create the model
+const User= mongoose.model('User',userSchema);
+const elywin = new User ({name:'elywin'});
+
+//save model
+elywin.save(function(err,elywin){
+    if(err) return console.error(err);
+    console.log("Document inserted succussfully!");
+});
